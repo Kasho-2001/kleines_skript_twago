f5d42ebd4bc18f03793f5251c7c53a4bc086393d
Kasho-2001 <you@example.com>
2021-11-25 21:03:45 +0100

    was gehtn

--- verschiebe_datei.py
+++ verschiebe_datei.py
@@ -2,25 +2,26 @@ import os
 from tkinter import Tk     # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename
 import shutil
-
+from tkinter import simpledialog
 
 
 #Variablendefinition
-old_target_number = '123'
-old_target_directory = ('.')
-new_target_directory = ('/new_dir')
-new_target_filename =  ('neindasistdieneuedatei.txt')
+ausgangsverzeichnis = ('/home/kasho/Documents/twago/kleines_skript/quelle')         #Quelle der Dateien
+zielverzeichnis = ('neues_verzeichnis/')                                            #Ziel der Datei 
+zieldateiname =  ('dieneuedatei.txt')                                               #Name der Zieldatei 
+
+while True:
+    
+    #Eingabe
+    ROOT = Tk()
+    ROOT.withdraw()
+    target_number = simpledialog.askstring(title="Nummerneingabe", prompt="Bitte hier die Auftragsnummer eingeben:")
+    print("Auftragsnummer: ", target_number)
 
 
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = askopenfilename(filetypes=[('all including old_target_number', '*'+old_target_number+'*')]) # show an "Open" dialog box and return the path to the selected file
-print(filename)
-shutil.copy(filename,new_target_directory+new_target_filename)
+    #Dateienauswahl
+    filename = askopenfilename(initialdir = ausgangsverzeichnis, filetypes=[('allle mit >target_number<', '*'+target_number+'*')])
+    print(filename)
 
-all_files = os.listdir(old_target_directory)
-print(all_files)
-for i in all_files:
-    #print(i) 
-    position = i.find(old_target_number)
-    if position != -1:
-        print(i)
+    #Kopieren
+    shutil.copy(filename,zielverzeichnis+zieldateiname)
