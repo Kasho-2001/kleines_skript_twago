166a32c36dcb11057290e5fcf7fe532a8fe33c8c
Kasho-2001 <you@example.com>
2021-11-25 22:15:27 +0100

    jetzt mit seperatem config.ini file

--- verschiebe_datei.py
+++ verschiebe_datei.py
@@ -3,13 +3,20 @@ from tkinter import Tk     # from tkinter import Tk for Python 3.x
 from tkinter.filedialog import askopenfilename
 import shutil
 from tkinter import simpledialog
+from configparser import ConfigParser
 
+
+#config.ini auslesen und Variablen übergeben
+config_object = ConfigParser()
+config_object.read("config.ini")
+
+variablen = config_object["variablen"]
 
 #Variablendefinition
-ausgangsverzeichnis = ('/home/kasho/Documents/twago/kleines_skript/quelle')         #Quelle der Dateien
-zielverzeichnis = ('neues_verzeichnis/')                                            #Ziel der Datei 
-zieldateiname =  ('dieneuedatei.txt')                                               #Name der Zieldatei 
-
+ausgangsverzeichnis = variablen["ausgangsverzeichnis"]                              #Quelle der Dateien
+zielverzeichnis = variablen["zielverzeichnis"]                                      #Ziel der Datei 
+zieldateiname =  variablen["zieldateiname"]                                         #Name der Zieldatei 
+print(ausgangsverzeichnis,'83')
 while True:
     
     #Eingabe
